987bfcef081834f1431bd43b4e007a2ff3abe78b
Evaldo de Souza <evaldo@MacBook-Pro-de-Evaldo.local>
2020-06-17 09:19:30 -0300

    Create dando erro

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Evaldo de Souza',
-    email: 'evaldocz18@gmail.com',
-    password_hash: '123456',
-  });
-  console.log(user);
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
