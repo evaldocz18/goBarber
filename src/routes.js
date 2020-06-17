1ab9d2959d23a2209d319b4d73d473f725e0d938
Evaldo de Souza <evaldo@MacBook-Pro-de-Evaldo.local>
2020-06-16 21:08:31 -0300

    Teste

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Evaldo' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Evaldo de Souza',
+    email: 'evaldocz18@gmail.com',
+    password_hash: '123456',
+  });
+  console.log(user);
+  return res.json(user);
 });
 
 export default routes;
