6ca6ceff6c6fb3e2dc82e421cfcec4cf6720ce6f
Evaldo de Souza <evaldo@MacBook-Pro-de-Evaldo.local>
2020-06-19 11:34:50 -0300

    Retornando ID via token

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-console */
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+    // eslint-disable-next-line no-console
+    console.log(decoded);
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
