650640ac4f54ef83fb06b460812e8150681e0b28
Evaldo de Souza <evaldo@MacBook-Pro-de-Evaldo.local>
2020-06-22 10:23:59 -0300

    MID

--- src/routes.js
+++ src/routes.js
@@ -10,7 +10,8 @@ const routes = new Router();
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
+routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
-// routes.use(authMiddleware);
 
 export default routes;
