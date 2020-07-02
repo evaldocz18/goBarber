f10577e535e43e598efde0a7528ec9741053cdd8
Evaldo de Souza <evaldo@MacBook-Pro-de-Evaldo.local>
2020-07-02 20:24:38 -0300

    Configurando fila com Redis

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import { resolve } from 'path';
+// import { resolve } from 'path';
 import exphbs from 'express-handlebars';
 import nodemailerhbs from 'nodemailer-express-handlebars';
 import mailConfig from '../config/mail';
